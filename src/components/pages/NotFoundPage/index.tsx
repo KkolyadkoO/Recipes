cbd683a70b73d41cd4fb485257cbc5a33b1692f7
KkolyadkoO <artem.7ko52@gmail.com>
2024-12-18 01:35:20 +0300

    Адаптив всего кроме header

---
+++ src/components/pages/NotFoundPage/index.tsx
@@ -0,0 +1,29 @@
+﻿import Header from "../../Header";
+import React from "react";
+import Footer from "../../Footer";
+import { observer } from "mobx-react-lite";
+import { Link } from "react-router-dom";
+import "./styles.css"; // Подключаем CSS файл для стилей
+
+const NotFoundPage = () => {
+    return (
+        <div className="wrapper">
+            <Header />
+            <div className="content notFoundPage">
+                <div className="notFoundPage__content">
+                    <h1 className="notFoundPage__title">404</h1>
+                    <p className="notFoundPage__message">Ой, такой страницы нет!</p>
+                    <p className="notFoundPage__description">
+                        Возможно, она была удалена или вы ввели неправильный адрес.
+                    </p>
+                    <Link to="/" className="notFoundPage__button">
+                        Вернуться на главную
+                    </Link>
+                </div>
+            </div>
+            <Footer />
+        </div>
+    );
+};
+
+export default observer(NotFoundPage);
--- src/components/route/appRoutes.tsx
+++ src/components/route/appRoutes.tsx
@@ -6,6 +6,7 @@ import RecipePage from "../pages/RecipePage";
 import LoginPage from "../pages/LoginPage";
 import RegisterPage from "../pages/RegisterPage";
 import MyRecipesPage from "../pages/MyRecipesPage";
+import NotFoundPage from "../pages/NotFoundPage";
 
 
 
@@ -21,6 +22,7 @@ const AppRoutes = () => {
                 <Route path="/login" element={<LoginPage/>}> </Route>
                 <Route path="/register" element={<RegisterPage/>}> </Route>
                 <Route path="/myrecipes" element={<MyRecipesPage/>}> </Route>
+                <Route path="*" element={<NotFoundPage />} />
             </Routes>
         </Router>
     );
