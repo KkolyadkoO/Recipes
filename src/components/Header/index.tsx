f013fc46831eabd7436684ef1a908dc999ed45d5
KkolyadkoO <artem.7ko52@gmail.com>
2024-12-19 18:30:18 +0300

    Adaptive header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,27 +1,42 @@
-﻿import {observer} from "mobx-react-lite";
-import React from "react";
+﻿import { observer } from "mobx-react-lite";
+import React, { useState } from "react";
 import './styles.css';
-import {Link} from "react-router-dom";
 
 const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
         <header className="header">
-            <div className="logo">
-                recipes
-            </div>
+            <div className="logo">recipes</div>
             <nav className="nav">
                 <ul>
-                    <li><a href="#" className='header__menu__text'>главная</a></li>
-                    <li><a href="#" className='header__menu__text'>о нас</a></li>
-                    <li><a href="#" className='header__menu__text'>предложения</a></li>
-                    <li><a href="#" className='header__menu__text'>популярное</a></li>
+                    <li><a href="#" className="header__menu__text">главная</a></li>
+                    <li><a href="#" className="header__menu__text">о нас</a></li>
+                    <li><a href="#" className="header__menu__text">предложения</a></li>
+                    <li><a href="#" className="header__menu__text">популярное</a></li>
                 </ul>
             </nav>
             <div className="user-actions">
-                <button className="login-button header__menu__text" >Войти</button>
-                {/*<div className="user-icon">*/}
-                {/*    <i className="fa fa-user-circle" aria-hidden="true"></i>*/}
-                {/*</div>*/}
+                <button className="login-button header__menu__text">Войти</button>
+            </div>
+            {/* Бургер-меню */}
+            <div className="burger-menu" onClick={toggleMenu}>
+                <i className="burger-menu__icon"><img className="header__icons" src="/images/burger-bar.png"/></i>
+            </div>
+            {/* Боковое меню */}
+            <div className={`side-menu ${menuOpen ? "open" : ""}`}>
+                <button className="close-btn" onClick={toggleMenu}><img className="header__icons" src="/images/close.png"/></button>
+                <ul>
+                <li><a href="#" className="header__menu__text">главная</a></li>
+                    <li><a href="#" className="header__menu__text">о нас</a></li>
+                    <li><a href="#" className="header__menu__text">предложения</a></li>
+                    <li><a href="#" className="header__menu__text">популярное</a></li>
+                </ul>
+                <button className="login-button header__menu__text">Войти</button>
             </div>
         </header>
     );
